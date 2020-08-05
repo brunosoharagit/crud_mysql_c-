2b3d06c4f8fc6fbcff1cef6045ee22a94a0151a0
brunosoahragit <b.sohara.git@gmail.com>
2020-08-05 12:38:06 -0300

    commit de arquivos

---
+++ exemplo_crud/exemplo_crud/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace exemplo_crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbCode = new System.Windows.Forms.TextBox();
+            this.txbAddress = new System.Windows.Forms.TextBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.cmbState = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(208, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Código";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Endereço";
+            // 
+            // txbCode
+            // 
+            this.txbCode.Location = new System.Drawing.Point(100, 58);
+            this.txbCode.Name = "txbCode";
+            this.txbCode.Size = new System.Drawing.Size(39, 20);
+            this.txbCode.TabIndex = 5;
+            // 
+            // txbAddress
+            // 
+            this.txbAddress.Location = new System.Drawing.Point(123, 169);
+            this.txbAddress.Name = "txbAddress";
+            this.txbAddress.Size = new System.Drawing.Size(214, 20);
+            this.txbAddress.TabIndex = 6;
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Location = new System.Drawing.Point(100, 133);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(91, 21);
+            this.cmbCity.TabIndex = 7;
+            // 
+            // cmbState
+            // 
+            this.cmbState.FormattingEnabled = true;
+            this.cmbState.Location = new System.Drawing.Point(293, 133);
+            this.cmbState.Name = "cmbState";
+            this.cmbState.Size = new System.Drawing.Size(44, 21);
+            this.cmbState.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Nome";
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(100, 94);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(239, 20);
+            this.txbName.TabIndex = 10;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(20, 254);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(99, 23);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "Inserir";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Cadastro";
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(146, 58);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(56, 19);
+            this.btnQuery.TabIndex = 13;
+            this.btnQuery.Text = "Buscar";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(146, 254);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Remover";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(146, 293);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(20, 293);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(99, 23);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Alterar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 338);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbState);
+            this.Controls.Add(this.cmbCity);
+            this.Controls.Add(this.txbAddress);
+            this.Controls.Add(this.txbCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbCode;
+        private System.Windows.Forms.TextBox txbAddress;
+        private System.Windows.Forms.ComboBox cmbCity;
+        private System.Windows.Forms.ComboBox cmbState;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ exemplo_crud/exemplo_crud/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace exemplo_crud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbState.Items.Add("SP");
+            cmbState.Items.Add("PR");
+            cmbState.Items.Add("BA");
+
+            cmbCity.Items.Add("Campinas");
+            cmbCity.Items.Add("Curitiba");
+            cmbCity.Items.Add("Salvador");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection cn = new MySqlConnection("server=localhost; username=bruno; database=exemplodb; password=dbadmin");
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into tabela (codigo, nome, cidade, estado, endereco) values (null, ?, ?, ?, ?)",cn);
+
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar, 60).Value = txbName.Text;
+                cmd.Parameters.Add("@cidade", MySqlDbType.VarChar, 28).Value = cmbCity.SelectedItem.ToString();
+                cmd.Parameters.Add("@estado", MySqlDbType.VarChar, 2).Value = cmbState.SelectedItem.ToString();
+                cmd.Parameters.Add("@enderco", MySqlDbType.VarChar, 100).Value = txbAddress.Text;
+
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Dados inseridos com sucesso!!");
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show("Não connectou. Erro: " + error);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection cn = new MySqlConnection("server=localhost; username=bruno; database=exemplodb; password=dbadmin");
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("update tabela set nome = ?, cidade = ?, estado = ?, endereco = ? where codigo = ?", cn);
+
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar, 60).Value = txbName.Text;
+                cmd.Parameters.Add("@cidade", MySqlDbType.VarChar, 28).Value = cmbCity.SelectedItem.ToString();
+                cmd.Parameters.Add("@estado", MySqlDbType.VarChar, 2).Value = cmbState.SelectedItem.ToString();
+                cmd.Parameters.Add("@endereco", MySqlDbType.VarChar, 100).Value = txbAddress.Text;
+                cmd.Parameters.Add("@codigo", MySqlDbType.Int32, 10).Value = txbCode.Text;
+
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Dados alterados com sucesso!!");
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show("Não foi possível alterar este(s) dado(s). \nErro: " + error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection cn = new MySqlConnection("server=localhost; username=bruno; database=exemplodb; password=dbadmin");
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete from tabela where codigo = ?", cn);
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add("@codigo", MySqlDbType.Int32, 10).Value = txbCode.Text;
+
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Dados removidos com sucesso!!");
+                cn.Close();
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show("Erro ao remover um dado. \nErro: " + error);
+            }
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection cn = new MySqlConnection("server=localhost; username=bruno; database=exemplodb; password=dbadmin");
+                cn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select nome, cidade, estado, endereco from tabela where codigo = ?", cn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = txbCode.Text;
+
+                cmd.CommandType = CommandType.Text;
+
+                MySqlDataReader dr;
+                dr = cmd.ExecuteReader();
+                dr.Read();
+
+                txbName.Text = dr.GetString(0);
+                cmbCity.Text = dr.GetString(1);
+                cmbState.Text = dr.GetString(2);
+                txbAddress.Text = dr.GetString(3);
+
+                cn.Close();
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show("Erro na busca do registro. \nErro: " + error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txbCode.Text = "";
+            txbAddress.Text = "";
+            cmbCity.Text = "";
+            cmbState.Text = "";
+            txbName.Text = "";
+        }
+    }
+}
